f2106d0984730b8e9605a672b07dde57fcc802e3
Nathan Jones <thenathanjones@gmail.com>
2013-05-25 08:54:45 +0800

    Initial commit

---
+++ ElCamino/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElCamino.MvcApplication" Language="C#" %>
---
+++ ElCamino/Migrations/201305241416258_InitialSchema.cs
@@ -0,0 +1,26 @@
+namespace ElCamino.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialSchema : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "UserProfile",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("UserProfile");
+        }
+    }
+}
