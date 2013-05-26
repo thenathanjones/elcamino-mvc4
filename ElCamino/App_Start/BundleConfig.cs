db2a449092c0d1b6d9db2942621236f35bba6811
Nathan Jones <thenathanjones@gmail.com>
2013-05-26 20:22:06 +0800

    Vendor the JS

--- ElCamino/App_Start/BundleConfig.cs
+++ ElCamino/App_Start/BundleConfig.cs
@@ -9,18 +9,16 @@ namespace ElCamino
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/vendor/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/vendor/jquery.unobtrusive*",
+                        "~/Scripts/vendor/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+                        "~/Scripts/vendor/modernizr-*"));
 
         }
     }
